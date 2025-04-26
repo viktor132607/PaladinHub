2cdaca5967767165f1dc91e6af1534deb6a2981b
vikto <vikto@DESKTOP-5FK60IB>
2025-04-27 00:10:50 +0300

    Init

---
+++ PaladinProject/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using PaladinProject.Models;
+using System.Diagnostics;
+
+namespace PaladinProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HolyPaladinguide()
+        {
+            return View();
+        }
+        public IActionResult ProtectionPaladinguide()
+        {
+            return View();
+        }
+        public IActionResult RetributionPaladinGuide()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
